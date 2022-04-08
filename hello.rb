65331c9713db8b38c0aaac9470a898c7e198c5f8
koto <binsaobinsao77+1@gmail.com>
2022-04-08 11:23:47 +0000

    2022.04.08 kadai for ruby04 2nd

--- hello.rb
+++ hello.rb
@@ -138,20 +138,21 @@
 # animal.greet
 
 
-# Peopleクラスを作成してください
+# 課題=======================================================================================
+# 1.
 class People
 
-# Peopleクラスに「Peopleのインスタンスが作られました」と出力するコンストラクタを定義してください
+# 2.Peopleクラスに「Peopleのインスタンスが作られました」と出力するコンストラクタを定義してください
   def initialize
     p "Peopleのインスタンスが作られました"
   end
 
-# Peopleクラスに「私はPeopleクラスです」と出力するクラスメソッドを定義してください
+# 3.Peopleクラスに「私はPeopleクラスです」と出力するクラスメソッドを定義してください
   def self.greet
     p "私はPeopleクラスです"
   end
 
-# Peopleクラスに、インスタンス変数「@name」を定義し、「attr_accessor」メソッドでアクセスできるようにしてください
+# 4.Peopleクラスに、インスタンス変数「@name」を定義し、「attr_accessor」メソッドでアクセスできるようにしてください
   attr_accessor :name
 
 end
@@ -162,10 +163,10 @@ p people.name
 
 People.greet
 
-# Peopleクラスを継承して、ChildPeopleクラスを作成してください
+# 5.Peopleクラスを継承して、ChildPeopleクラスを作成してください
 class ChildPeople < People
 
-# ChildPeopleクラスに「私は目からビームが出せます」と出力するクラスメソッドを定義してください
+# 6.ChildPeopleクラスに「私は目からビームが出せます」と出力するクラスメソッドを定義してください
   def self.beam
     p "私は目からビームが出せます"
   end
